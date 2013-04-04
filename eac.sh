9f28f4f96e186f382623b7b96fc74751b5aaef2b
leo-bogert <github@leo.bogert.de>
2013-04-04 22:10:14 +0200

    Fix if-syntax

--- eac.sh
+++ eac.sh
@@ -8,9 +8,9 @@ if ! umount /mnt/cdrom &> /dev/null ; then
 	# No error, just make sure it was not already mounted.
 fi
 
-if ! mount /mnt/cdrom ; do
+if ! mount /mnt/cdrom ; then
 	notify-send --urgency=critical --category=device.error 'Starting Exact Audio copy failed!' 'You have to insert a data CDROM before starting EAC!'
-done
+fi
 
 WINEARCH=win32 wine ./.wine/drive_c/Program\ Files/Exact\ Audio\ Copy/EAC.exe
 
