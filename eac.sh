4606632ba1ddd6019463d44e1b2a2f46e5062bb8
leo-bogert <github@leo.bogert.de>
2013-05-10 18:46:19 +0200

    Make eac.sh automatically detect the number of /mnt/cdromX from the username

--- eac.sh
+++ eac.sh
@@ -4,17 +4,20 @@ if ! source "lib-bash-leo.sh" ; then
 	exit 1
 fi
 
-if ! umount /mnt/cdrom &> /dev/null ; then
+username="$(id --user --name)"
+drive_number="${username#ripper}"
+
+if ! umount "/mnt/cdrom$drive_number" &> /dev/null ; then
 	true # No error, just make sure it was not already mounted.
 fi
 
-if ! mount /mnt/cdrom ; then
+if ! mount "/mnt/cdrom$drive_number" ; then
 	notify-send --urgency=critical --category=device.error 'Starting Exact Audio copy failed!' 'You have to insert a data CDROM before starting EAC!'
 	exit 1
 fi
 
 WINEARCH=win32 wine ./.wine/drive_c/Program\ Files/Exact\ Audio\ Copy/EAC.exe
 
-if ! umount /mnt/cdrom &> /dev/null ; then
+if ! umount "/mnt/cdrom$drive_number" &> /dev/null ; then
 	true # The kernel has probably already auto-unmounted it
 fi
